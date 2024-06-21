a6c6cc99605f65f82e8dfbf71b4115305ecc36c3
MiKako <taufikqillah25@gmail.com>
2024-06-21 16:05:24 +0700

    The First file commit

---
+++ fb.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+//Script By Keito Klein
+exports.fb = async(url) => {
+    const header = {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'Origin': 'https://getmyfb.com',
+      'Referer': 'https://getmyfb.com/'
+    };
+  
+    const query = {
+      id: url,
+      locale: 'en'
+    };
+  
+    const res = await axios.post('https://getmyfb.com/process', qs.stringify(query), { header })
+      const $ = cheerio.load(res.data)
+      response = $(".results-download").find("ul > li:nth-child(1) > a").attr("href")
+      return response;
+    
+  };
